9e7efbaaa9ac67181687b02999e8f6919c73d345
Denis Gryaznov <distaboo@mail.ru>
2018-12-05 21:53:14 +0600

    1

--- script.py
+++ script.py
@@ -19,19 +19,19 @@ def echo_message(message):
     bot.reply_to(message, "Привет")
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
+TOKEN2 = '405823545:AAHT8vByxjot138gjLW4LYT1qIWvALIxFMM'
+bot2 = telebot.TeleBot(TOKEN2)
+
+
+
+@bot2.message_handler(commands=['start'])
+def start(message):
+    bot2.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot2.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot2.reply_to(message, "Привет")
 bot.polling()
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://damp-lake-81790.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot2.polling()
