365e0454666efc419ab070584bf19b0725dda07e
Denis Gryaznov <distaboo@mail.ru>
2018-12-05 21:03:44 +0600

    1

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, "Привет")
 
 
 #@server.route('/' + TOKEN, methods=['POST'])
