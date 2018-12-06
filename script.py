1dec1142cfc93f6f8fc251660a07492eaa1d06d9
Denis Gryaznov <distaboo@mail.ru>
2018-12-06 22:09:42 +0600

    2

--- script.py
+++ script.py
@@ -5,8 +5,19 @@ import requests
 import telebot
 
 TOKEN = '664435594:AAHUcl61psQuC5LvPqq7L-JZvMCmOX62SGU'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+secret = 'hjrt5344rekjhd4o3weks4'
+url = 'http://distaboo.pythonanywhere.com/'+secret
+bot = telebot.TeleBot(TOKEN, threaded = False)
+bot.remove_webhook()
+bot.set_webhook(url = url)
+
+app = Flask(__name__)
+@app.route('/'+secret, methods = ['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok',200
+
 
 
 @bot.message_handler(commands=['start'])
@@ -17,21 +28,3 @@ def start(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, "Привет")
-
-
-
-TOKEN2 = '405823545:AAHT8vByxjot138gjLW4LYT1qIWvALIxFMM'
-bot2 = telebot.TeleBot(TOKEN2)
-
-
-
-@bot2.message_handler(commands=['start'])
-def start(message):
-    bot2.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot2.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot2.reply_to(message, "Привет")
-bot2.polling()
-bot.polling()
